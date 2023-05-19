4f5ea12d2784c55456a8f0874217c2c8a3f087d2
kortkamp <marcelusmedius@gmail.com>
2023-05-19 13:18:18 -0300

    feat: add framwerok class

--- src/api/buildCurriculum.ts
+++ src/api/buildCurriculum.ts
@@ -1,28 +1,95 @@
 import ICurriculum from '@/types/ICurriculum';
-import { jsPDF as JsPDF } from 'jspdf';
+import jsPDF, { jsPDF as JsPDF } from 'jspdf';
 
 import '../assets/Poppins-Regular-normal';
+import '../assets/Poppins-Medium-normal';
 
 const pagePadding = {
   x: 10,
   y: 10,
 };
 
+const colorSchema = {
+  primary: '#2c3544',
+  neutral: {
+    DEFAULT: 'white',
+  },
+  text: {
+    contrast: {
+      default: 'white',
+      light: '#9599a2',
+    },
+  },
+  background: {
+    primary: '#2c3544',
+  },
+};
+
+interface ICoordinate {
+  x: number,
+  y: number
+}
+interface IDimension {
+  w: number,
+  h: number
+}
+interface IFrameworkOptions {
+  cursor?: ICoordinate
+  margin?: ICoordinate
+  page?: IDimension
+}
+class Framework {
+  public cursor:ICoordinate;
+
+  public margin: ICoordinate;
+
+  public pageDimensions: IDimension;
+
+  public page = 1;
+
+  private pdf: jsPDF;
+
+  constructor(
+    pdf: jsPDF,
+    {
+      cursor = { x: 0, y: 0 },
+      margin = { x: 10, y: 10 },
+      page = { h: 297, w: 210 },
+    }:IFrameworkOptions,
+  ) {
+    this.pdf = pdf;
+    this.cursor = cursor;
+    this.margin = margin;
+    this.pageDimensions = page;
+  }
+
+  write() {
+    this.cursor.x += 10;
+  }
+}
+
 const buildPDF = (curriculum: ICurriculum) => {
   const pdf = new JsPDF('p', 'mm');
 
-  pdf.setDrawColor('#2c3544');
-  pdf.setFillColor('#2c3544');
+  const document = new Framework(pdf, {});
+
+  // header
+  pdf.setDrawColor(colorSchema.background.primary);
+  pdf.setFillColor(colorSchema.background.primary);
   pdf.rect(0, 0, 210, 42, 'F');
+
   pdf.setFontSize(24);
-  pdf.setTextColor('white');
-  pdf.setFont('Poppins-Regular', 'normal');
-
-  console.log(pdf.getFontList());
-
+  pdf.setTextColor(colorSchema.neutral.DEFAULT);
+  pdf.setFont('Poppins-Medium');
   pdf.text(`${curriculum.personal?.name} ${curriculum.personal?.surname}` || '', pagePadding.x, pagePadding.y, { maxWidth: 300, baseline: 'top' });
-  pdf.setFontSize(20);
-  // pdf.text(`${curriculum.personal?.title}` || '', 10, 5, { maxWidth: 300, align: 'center' });
+
+  pdf.addPage();
+  pdf.setPage(1);
+  pdf.setTextColor(colorSchema.text.contrast.light);
+  pdf.setFontSize(12);
+  pdf.setFont('Poppins-Regular');
+
+  pdf.text(`${curriculum.personal?.title}` || '', pagePadding.x, pagePadding.y + 10, { maxWidth: 300, baseline: 'top' });
 
   return pdf.output();
 };
