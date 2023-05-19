3149b7126e322de17d935dce491783361a7dc67c
Marcelo Kortkamp <marcelusmedius@gmail.com>
2023-05-19 11:58:26 -0300

    feat: add fonts

--- src/api/buildCurriculum.ts
+++ src/api/buildCurriculum.ts
@@ -1,7 +1,7 @@
 import ICurriculum from '@/types/ICurriculum';
 import { jsPDF as JsPDF } from 'jspdf';
 
-import '../assets/Poppins-normal';
+import '../assets/Poppins-Regular-normal';
 
 const pagePadding = {
   x: 10,
@@ -16,7 +16,9 @@ const buildPDF = (curriculum: ICurriculum) => {
   pdf.rect(0, 0, 210, 42, 'F');
   pdf.setFontSize(24);
   pdf.setTextColor('white');
-  pdf.setFont('Poppins-normal', 'bold');
+  pdf.setFont('Poppins-Regular', 'normal');
+
+  console.log(pdf.getFontList());
 
   pdf.text(`${curriculum.personal?.name} ${curriculum.personal?.surname}` || '', pagePadding.x, pagePadding.y, { maxWidth: 300, baseline: 'top' });
   pdf.setFontSize(20);
